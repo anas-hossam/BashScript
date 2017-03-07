3c23638432b58acc87391211dc23a114ead12913
anas92 <softeng.anas@gmail.com>
2017-03-07 17:15:26 +0200

    Add files via upload

--- dbms.sh
+++ dbms.sh
@@ -1,44 +1,114 @@
 #!bin/bash
 
-select choice in Create_Database Rename_Database Drop_Database Create_Table Drop_Table Update_Table Delete_From_Table Select_From_Table 
+select choice in Create_Database Use_Database Rename_Database Drop_Database Create_Table Drop_Table Insert_To_Table Update_Table Delete_From_Table Select_From_Table
 do
 case $choice in
-Create_Database) 
+Create_Database)
 {
 	shopt -s extglob
 	read -p "Enter Database Name :"
 	mkdir $REPLY
 }
 ;;
-Rename_Database) 
+Use_Database)
 {
-	shopt -s extglob	
+	shopt -s extglob
+	read -p "Enter Database Name :"
+	cd $REPLY
+}
+;;
+Rename_Database)
+{
+	shopt -s extglob
 	read -p "Enter Database Name To Use :"
-	set -x 
+	set -x
 	olddb=$REPLY
 	shopt -s extglob
-	read -p "Enter New Database Name :" 
+	read -p "Enter New Database Name :"
 	newdb=$REPLY
 	set +x
 	mv $olddb $newdb
 }
 ;;
-Drop_Database) 
+Drop_Database)
 {
 	shopt -s extglob
 	read -p "Enter Database Name To DELETE :"
 	rm -r $REPLY
 }
 ;;
-Create_Table) echo Create Table.
+Create_Table)
+{
+	shopt -s extglob
+	read -p "Enter Name of Table?"
+	tableName=$REPLY
+	touch $tableName
+	shopt -s extglob
+	read -p "Enter Number of Columns?"
+	numCols=$REPLY
+	for(( i=0; i<$numCols ;i++ ))
+	do
+		shopt -s extglob
+		read -p "Enter name of column :"
+		colName=$REPLY
+		shopt -s extglob
+		read -p "Enter Type (int or string):"
+		colType=$REPLY
+		if((i==$numCols-1))
+		then
+		echo  "$colName:$colType">>$tableName
+		else
+		echo -n "$colName:$colType:">>$tableName
+		fi
+
+	done
+}
 ;;
-Drop_Table) echo Drop Table.
+Drop_Table)
+{
+	shopt -s extglob
+	read -p "Enter Table Name To DELETE :"
+	rm -r $REPLY
+}
+;;
+Insert_To_Table)
+{
+	shopt -s extglob
+	read -p "Enter Table Name : "
+	table=$REPLY
+	numOfFields=$(awk -F: 'END{print NF}' $table)
+	echo $numOfFields
+	arr=($(awk -F: '{ for ( i = 1 ; i <= NF; i++){ if (NR==1) print $i; } }' $table));
+
+	for (( i = 0; i < ${#arr[@]}; i++ ));
+	  do
+	 if ((i%2==0))
+	 then
+		echo "Enter "${arr[i]}" : ";
+		read
+		echo -n $REPLY":">>$table
+	else
+			if((i==${#arr[@]}-1))
+			then
+				echo " :">>$table
+			else
+				echo -n " :">>$table
+			fi
+	 fi
+	 done
+}
 ;;
 Update_Table) echo Update Table.
 ;;
-Delete_From_Table) echo Delete From Table.
+Delete_From_Table)
+{
+	echo Delete Table.
+}
 ;;
-Select_From_Table) echo Select From Table.
+Select_From_Table)
+{
+	echo select
+}
 ;;
 *) echo $REPLY is not one of the choices.
 ;;
